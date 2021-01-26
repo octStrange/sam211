6227aceac1973d0bbe6d7ed673ed38abc5c2b638
octStrange <oct_strange@outlook.com>
2021-01-27 01:34:01 +0530

    adding home link

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Welcome to Flask'
+    return 'Welcome to Flask <a href="/home">Click Here</a>'
+    
+@app.route('/home')
+def index():
+    return 'Home page'
 
 if __name__ == "__main__":
     app.run(debug=True)
